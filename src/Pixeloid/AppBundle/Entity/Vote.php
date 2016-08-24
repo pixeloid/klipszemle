d890b3790bcf78a592a19879cdbb9ad0cfefcd5f
Pixeloid Office <olah.gergely@pixeloid.hu>
2016-08-24 22:31:02 +0200

    Vote 2

---
+++ src/Pixeloid/AppBundle/Entity/Vote.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Pixeloid\AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Vote
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Vote
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created", type="datetime")
+     */
+    private $created;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     *
+     * @return Vote
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+}
+
