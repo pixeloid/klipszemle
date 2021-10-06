3ec48be4ebbf8fa4f491361384ee59a5df9d1470
Oláh Gergő <olah.gergely@pixeloid.hu>
2021-10-06 12:07:02 +0200

    fix jury

--- app/src/Menu/Builder.php
+++ app/src/Menu/Builder.php
@@ -98,9 +98,9 @@ class Builder implements ContainerAwareInterface
      //   $menu->addChild('Nevezés', array('route' => 'eventregistration_new', 'routeParameters' => array()))->setLinkAttribute('class', ' highlight');;
         // $menu->addChild('Mi a klipszemle?', array('uri' => '/#about'))->setLinkAttribute('class', 'animated page-scroll hidden-sm');
         // $menu->addChild('Zsűri & Szervezők', array('uri' => '/#jury'))->setLinkAttribute('class', 'animated page-scroll');
-        // $menu->addChild('Program', array('uri' => '/#program'))->setLinkAttribute('class', 'animated page-scroll');
+        //$menu->addChild('Program', array('uri' => '/#program'))->setLinkAttribute('class', 'animated page-scroll');
         // $menu->addChild('Kapcsolat', array('uri' => '/#contact'))->setLinkAttribute('class', 'animated page-scroll');
-     //   $menu->addChild('Program', array('route' => 'program'));
+        $menu->addChild('Program', array('route' => 'program'));
      //   $menu->addChild('Hírek', array('route' => 'news'));
         // $menu->addChild('Rólunk írták', array('uri' => '#'));
         // $menu->addChild('Cuccok', array('uri' => '#'));
