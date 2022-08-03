ffe553163332bbc84869ad240384c5d56abb14fc
pixeloid <olah.gergely@pixeloid.hu>
2022-08-03 08:31:35 +0200

    6.1

--- app/src/Controller/SecurityController.php
+++ app/src/Controller/SecurityController.php
@@ -5,8 +5,8 @@ namespace App\Controller;
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -14,15 +14,19 @@ class SecurityController extends AbstractController
     /**
      * @Route("/ps/{pass}", name="app_ps")
      * @param $pass
-     * @param UserPasswordEncoderInterface $encoder
+     * @param UserPasswordHasherInterface $passwordHasher
      * @return Response
      */
     public function ps(
         $pass,
-        UserPasswordEncoderInterface $encoder
+        UserPasswordHasherInterface $passwordHasher
     ): Response {
         $user = new User();
-        $encoded = $encoder->encodePassword($user, $pass);
+
+        $encoded = $passwordHasher->hashPassword(
+            $user,
+            $pass
+        );
 
         die($encoded);
     }
@@ -45,6 +49,7 @@ class SecurityController extends AbstractController
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        throw new \LogicException(
+            'This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
