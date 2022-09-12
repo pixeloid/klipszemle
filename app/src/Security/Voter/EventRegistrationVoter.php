793c3c47fd24f6e895f3d8d506a74995a9abd641
pixeloid <olah.gergely@pixeloid.hu>
2022-09-12 13:05:14 +0200

    vote update

--- app/src/Security/Voter/EventRegistrationVoter.php
+++ app/src/Security/Voter/EventRegistrationVoter.php
@@ -6,12 +6,19 @@ use App\Entity\EventRegistration;
 use DateTime;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class EventRegistrationVoter extends Voter
 {
     public const CREATE = 'EVENTREGISTRATION_CREATE';
     public const VOTE = 'EVENTREGISTRATION_VOTE';
+    private Security $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
 
     protected function supports(string $attribute, $subject): bool
     {
@@ -23,11 +30,15 @@ class EventRegistrationVoter extends Voter
     protected function voteOnAttribute(string $attribute, $subject, TokenInterface $token): bool
     {
         $user = $token->getUser();
+
         // if the user is anonymous, do not grant access
         if (!$user instanceof UserInterface) {
             return false;
         }
 
+        if ($this->security->isGranted('ROLE_ADMIN')) {
+            return true;
+        }
         // ... (check conditions and return true to grant permission) ...
         if ($attribute == self::CREATE) {
             $today = new DateTime();
